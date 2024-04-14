eabfe4c84142a8b5c90746891162842c4fa91369
sivog <sivograkovkirill@mail.ru>
2024-04-14 03:04:24 +0300

    add logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import museumLogo from "@/assets/museumLogo.png";
+
+import {
+    LogoImage,
+    Logo,
+    LogoName,
+} from './styled';
+
+export interface LogotypeProps {
+    color: string
+}
+
+export const Logotype = ({color}: LogotypeProps) => {
+    return (
+        <Logo>
+            <LogoImage alt='museum' src={museumLogo}/>
+            <LogoName color={color}> Museum of <span>Art</span></LogoName>
+        </Logo>
+    )
+}
---
+++ src/components/Logo/styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 206px;
+  height: 63px;
+`;
+
+export const LogoName = styled.div`
+  display: flex;
+  align-self: end;
+  padding: 5px;
+  color: ${(props) => props.color};
+  span {
+    margin-left: 5px;
+    font-weight: bold;
+    color: #e0a449;
+  }
+`;
+
+export const LogoImage = styled.img`
+  width: 60px;
+  height: 70px;
+`;

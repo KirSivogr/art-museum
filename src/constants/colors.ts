d4948d7180464efc5a226925377419a6191b68fd
sivog <sivograkovkirill@mail.ru>
2024-04-26 17:46:06 +0300

    add colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+    primary: 'rgb(57, 57, 57)',
+    secondary: 'rgb(241, 121, 0)',
+    background: '#F9F9F9',
+    gold: '#FBD7B24D',
+    white: '#ffffff',
+    gradint: 'linear-gradient(90deg, #343333 38.05%, #484848 69.22%, #282828 98.98%)',
+    yellow: '#e0a449',
+    black: '#000',
+    grey: '#3939390d'
+}

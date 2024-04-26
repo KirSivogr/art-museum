021c7ea398edea2cca4ca722f374dd149e03ac53
sivog <sivograkovkirill@mail.ru>
2024-04-26 17:48:55 +0300

    add burger menu

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -6,14 +6,14 @@ import { Logo, LogoImage, LogoName } from './styled';
 
 export interface LogotypeProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Logotype = ({ color }: LogotypeProps) => {
+export const Logotype = ({ color, onClick }: LogotypeProps) => {
   return (
-    <Logo>
+    <Logo onClick={onClick}>
       <LogoImage alt='museum' src={museumLogo} />
       <LogoName color={color}>
-        {' '}
         Museum of <span>Art</span>
       </LogoName>
     </Logo>
